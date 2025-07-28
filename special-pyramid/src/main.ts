ee5e484ae5c785fb8b91b74ede9157657ea05025
awoyale dolapo <awoyaledolapo33@gmail.com>
2025-07-28 14:06:27 +0100

    initial commit

---
+++ special-pyramid/src/main.ts
@@ -0,0 +1,12 @@
+const  row: number  = 9;
+
+for (let i =1 ; i<=row ;i++){
+  let str :string ="";
+  for(let j=0 ; j<=row-1-i ; j++){
+     str+=" "
+  }
+  for (let j=0; j<i;j++){
+    str+=" "
+  }
+  console.log(str)
+}
