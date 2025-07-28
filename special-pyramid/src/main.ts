6f945d211043cdc20e9b9ba4eab8e030adc5ab0a
awoyale dolapo <awoyaledolapo33@gmail.com>
2025-07-28 14:40:52 +0100

    updated

--- special-pyramid/src/main.ts
+++ special-pyramid/src/main.ts
@@ -1,12 +1,13 @@
 const  row: number  = 9;
-
+let num: number = 1;
 for (let i =1 ; i<=row ;i++){
   let str :string ="";
   for(let j=0 ; j<=row-1-i ; j++){
      str+=" "
   }
   for (let j=0; j<i;j++){
-    str+="* "
+    str+=num + " ";
+    num++;
   }
   console.log(str)
  
